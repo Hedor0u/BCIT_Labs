248fbc099c1588aa1278e1bc9cbac3059ef2dee0
Hedor0u <aror991@mail.ru>
2017-12-29 13:04:47 +0300

    Lab

---
+++ 3sem_dz/3sem_dz/ClassLibrary/SqlEvent.fs
@@ -0,0 +1,40 @@
+﻿namespace WCFhostByWpfFsharpOnly
+
+open System
+open System.ServiceModel
+open System.Collections.Generic
+open System.Runtime.Serialization
+open System.Runtime.InteropServices 
+open System.Data
+open System.Text
+open DataCommonFsharp
+
+
+[<DataContract>]
+type SqlData = {
+                 [<DataMember>] mutable Sql : string
+               }
+
+[<ServiceContract>]
+type ISqlEvent = 
+    [<OperationContract(IsOneWay=false)>]
+    abstract member SqlEvent : value : SqlData -> DataSet
+    [<OperationContract>]
+    abstract member ServiceOK : unit -> string
+
+type SqlEvent() =
+    interface ISqlEvent with
+        member x.SqlEvent(md: SqlData) = let dc = new DataCommon()  // SET CONNECTION STRING below
+                                         do dc.ConnectionString <- "Persist Security Info=False;Integrated Security=true; Initial Catalog=XXXXXX; Server=XXXXXX"
+                                         let ds = dc.GetDataSet(new StringBuilder(md.Sql))
+                                         ds
+        member x.ServiceOK() = "OK" 
+
+
+
+
+
+
+
+
+   
---
+++ 3sem_dz/3sem_dz/WpfMsSql.xaml.fs
@@ -0,0 +1,44 @@
+﻿namespace WCFhostByWpfFsharpOnly
+
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+open System.Windows.Controls
+open System.IO
+open System.Windows.Markup
+open System.Reflection
+open Utilities
+open Microsoft.FSharp.Control
+open System.Windows
+open DataCommonFsharp
+
+
+type  WpfMsSql()  as this = 
+    inherit UserControl()
+    // XAML - MUST be Embedded Resource
+    let mySr = new StreamReader(Assembly.Load("_3sem_dz").GetManifestResourceStream("WpfMsSql.xaml"))
+    do this.Content <- XamlReader.Load(mySr.BaseStream):?> UserControl 
+
+    let mutable btnStart : Button  = this.Content?btnStart
+    let mutable btnStop : Button  = this.Content?btnStop
+
+    let mutable host : ServiceHost = null 
+    let startServer() =
+        let baseAddress = new Uri("http://localhost:8080/WindowsServiceSQL")   
+        do host <- new ServiceHost(typeof<SqlEvent>,  [|baseAddress|])
+        do host.Description.Behaviors.Add(new ServiceMetadataBehavior(HttpGetEnabled = true))
+        do host.AddServiceEndpoint(typeof<ISqlEvent>, new WSHttpBinding(),  baseAddress) |> ignore
+       
+        try
+           do host.Open()
+           do btnStart.IsEnabled <- false
+        with
+           | _ -> MessageBox.Show("You Must Run Application as Administrator.") |> ignore
+ 
+                     
+
+    do btnStart.Click.Add(fun _ -> do startServer())   // See file SqlEvent.fs - setup connection string ...first ..
+    do btnStop.Click.Add(fun _ -> if not (isNull host) && not btnStart.IsEnabled  then  do host.Close()  // host MUST be opened !
+                                  do Environment.Exit(0))
+
+
